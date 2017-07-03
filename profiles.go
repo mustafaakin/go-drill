38e847a87047ebe6b619211b4db5f7f0a518ddad
FURKAN BAYANSAR <e1881051@ceng.metu.edu.tr>
2017-07-03 22:27:18 +0300

    Implementation and test of 'Profiles' and 'Storage Plugins'

--- profiles.go
+++ profiles.go
@@ -15,8 +15,26 @@ type Profile struct {
 	User     string
 }
 
+// Profiles returns all running and finished queries as Profile format array
 func (d *Drillbit) Profiles() (*Profiles, error) {
 	var res Profiles
 	err := d.get("/profiles.json", &res)
+	return &res, err
+}
+
+// GetProfile given a query id, returns query profile
+func (d *Drillbit) GetProfile(queryId string) (interface{}, error) {
+	var res interface{}
+	path := "/profiles/" + queryId + ".json"
+	err := d.get(path, &res)
+	return &res, err
+}
+
+// DeleteProfile given a query id, returns success if the query is running, otherwise
+// it returns an error as a string so it cannot be decoded
+func (d *Drillbit) DeleteProfile(queryId string) (interface{}, error) {
+	var res ResultState
+	path := "/profiles/cancel/" + queryId
+	err := d.get(path, &res)
 	return &res, err
 }
--- profiles_test.go
+++ profiles_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func TestDrillbit_Profiles(t *testing.T) {
-	d, err := NewDrillbit("http://172.16.3.17:8047")
+	d, err := NewDrillbit("http://localhost:8047")
 	assert.NoError(t, err)
 	assert.NotNil(t, d)
 
@@ -19,4 +19,42 @@ func TestDrillbit_Profiles(t *testing.T) {
 	enc := json.NewEncoder(os.Stdout)
 	enc.SetIndent("", "  ")
 	enc.Encode(res)
+}
+
+func TestDrillbit_GetProfile(t *testing.T) {
+	d, err := NewDrillbit("http://localhost:8047")
+	assert.NoError(t, err)
+	assert.NotNil(t, d)
+
+	// get all profiles to obtain one of id
+	res, err := d.Profiles()
+	assert.NoError(t, err)
+	assert.NotNil(t, res)
+
+	result, err := d.GetProfile(res.FinishedQueries[0].QueryID)
+	assert.NoError(t, err)
+	assert.NotNil(t, result)
+
+	enc := json.NewEncoder(os.Stdout)
+	enc.SetIndent("", "  ")
+	enc.Encode(result)
+}
+
+func TestDrillbit_DeleteProfile(t *testing.T) {
+	d, err := NewDrillbit("http://localhost:8047")
+	assert.NoError(t, err)
+	assert.NotNil(t, d)
+
+	// get all profiles to obtain one of id
+	res, err := d.Profiles()
+	assert.NoError(t, err)
+	assert.NotNil(t, res)
+
+	result, err := d.DeleteProfile(res.FinishedQueries[0].QueryID)
+	assert.NoError(t, err)
+	assert.NotNil(t, result)
+
+	enc := json.NewEncoder(os.Stdout)
+	enc.SetIndent("", "  ")
+	enc.Encode(result)
 }
